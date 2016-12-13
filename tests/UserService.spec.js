79241390d115ee8bb1ed4003ccd4fa8399c20715
eutopian <eutopia@gmail.com>
2016-12-13 14:16:45 -0500

    Update UserService.spec.js

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,14 +1,14 @@
 describe('UserService', function () {
-	beforeEach(module('app'));
-	var UserService, $httpBackend;
+  beforeEach(module('app'));
+  var UserService, $httpBackend;
 
-	beforeEach(inject(function ($injector) {
-		UserService = $injector.get('UserService');
-		$httpBackend = $injector.get('$httpBackend');
-		$httpBackend.when('GET', '/rest/user').respond({user: 'Bill Gates', email: 'bill@microsoft.com'});
+  beforeEach(inject(function ($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', '/rest/user').respond({user: 'Bill Gates', email: 'bill@microsoft.com'});
   }));
 
-	it('should get the current users information', function (done) {
+  it('should get the current users information', function (done) {
     $httpBackend.expectGET('/rest/user');
 
     UserService
@@ -18,8 +18,8 @@ describe('UserService', function () {
         if (data.email === 'bill@microsoft.com' && data.user === 'Bill Gates') {
           done();
       	}
-    	});
+    });
 
     $httpBackend.flush();
-  	});
+  });
 });
